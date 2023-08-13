e74bfbb9a03452fece60254dac6e2ccf9589711c
c9u11 <tjrdud6412@naver.com>
2023-08-14 00:23:25 +0900

    ✨ Route Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  ROUTES_BASENAME,
+  ROUTES_PATH_HOME,
+  ROUTES_PATH_LOGIN,
+  ROUTES_PATH_ROOT,
+} from "./constants/Routes";
+import Home from "./pages/Home";
+import Login from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={ROUTES_BASENAME}>
+      <Routes>
+        <Route path={ROUTES_PATH_HOME} element={<Home />} />
+        <Route path={ROUTES_PATH_LOGIN} element={<Login />} />
+        <Route
+          path={ROUTES_PATH_ROOT}
+          element={<Navigate to={ROUTES_PATH_HOME} />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/constants/Routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES_BASENAME = process.env.PUBLIC_URL;
+export const ROUTES_PATH_ROOT = '/';
+export const ROUTES_PATH_LOGIN = '/login';
+export const ROUTES_PATH_HOME = '/home';

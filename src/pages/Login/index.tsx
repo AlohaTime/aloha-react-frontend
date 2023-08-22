c26b2d49dc0439f97cfd0e20ed07800d3919b673
c9u11 <tjrdud6412@naver.com>
2023-08-22 10:41:26 +0900

    ✏️ 문자 수정
    - login > Login
    - ts > tsx

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Container, Link, LoginButton, StretchedTextInput } from "./styled";
+
+
+function Login() {
+  const [id, setId] = useState("");
+  const [idErr, setIdErr] = useState("");
+  const [pw, setPw] = useState("");
+  const [pwErr, setPwErr] = useState("");
+
+  return (
+    <Container>
+      <h1>Aloha Time</h1>
+      <StretchedTextInput
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+        errorMessage={idErr}
+        placeholder="학번"
+      ></StretchedTextInput>
+      <StretchedTextInput
+        value={pw}
+        onChange={(e) => setPw(e.target.value)}
+        errorMessage={pwErr}
+        placeholder="비밀번호"
+        type="password"
+      ></StretchedTextInput>
+      <LoginButton>로그인</LoginButton>
+      <Link>회원가입</Link>
+      <Link>ID/PW 찾기</Link>
+    </Container>
+  );
+}
+
+export default Login;
---
+++ src/pages/login/styled.tsx
@@ -0,0 +1,29 @@
+import { styled } from "styled-components";
+import { ButtonStyle, TextInput } from "../../components/Input";
+
+export const Container = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  width: 100vw;
+  height: 100vh;
+  padding: 20px 50px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  align-self: stretch;
+`;
+
+export const StretchedTextInput = styled(TextInput)`
+  width: 100%;
+`;
+
+export const LoginButton = styled(ButtonStyle)`
+  width: 100%;
+  margin-top: 12px;
+`;
+
+export const Link = styled.a`
+  font-size: 12px;
+  color: #333;
+`;

bf2a4a9c15d204e376e5ccdeb26635cd52a1da8a
c9u11 <tjrdud6412@naver.com>
2023-08-22 10:33:31 +0900

    ✏️ login to Login

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {
   ROUTES_PATH_ROOT,
 } from "./constants/Routes";
 import Home from "./pages/Home";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 
 function App() {
   return (

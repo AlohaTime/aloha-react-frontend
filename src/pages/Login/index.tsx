eab2e1f0b0838d22a2ac9db0eeba6671c12cb773
c9u11 <tjrdud6412@naver.com>
2023-09-11 00:03:26 +0900

    ✨ Login Logic without API

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -7,12 +7,28 @@ import {
   Message,
   StretchedTextInput,
 } from "./styled";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
   const [id, setId] = useState("");
   const [idErr, setIdErr] = useState("");
   const [pw, setPw] = useState("");
   const [pwErr, setPwErr] = useState("");
+
+  const login = () => {
+    if (id === "") {
+      setIdErr("아이디를 입력해주세요.");
+      return;
+    }
+    else setIdErr("");
+    if (pw === "") {
+      setPwErr("비밀번호를 입력해주세요.");
+      return;
+    }
+    else setPwErr("");
+    navigate("/home");
+  }
 
   return (
     <Container>
@@ -30,7 +46,7 @@ function Login() {
         placeholder="비밀번호"
         type="password"
       ></StretchedTextInput>
-      <LoginButton>로그인</LoginButton>
+      <LoginButton onClick={login}>로그인</LoginButton>
       <Link>아이디/비밀번호 찾기</Link>
       <Message>
         Aloha Time은 인하대학교 학생만 사용이 가능합니다.<br />

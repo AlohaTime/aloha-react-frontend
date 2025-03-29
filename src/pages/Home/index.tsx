f02804f22409e9cc4aaff275990580d50d6ed019
c9u11 <tjrdud6412@naver.com>
2025-03-30 01:35:13 +0900

    🐛 과목 명 선택 시 특정 상황에서 아무런 문자도 안나오는 버그 수정
    - [로 시작하는 경우 아무런 문자도 나오지않아 그럴 때는 전체가 보이도록 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ import {
   LeftContainer,
   RefreshButton,
   RightContainer,
-  StretchedList,
+  StretchedList
 } from "./styled";
 import { Select } from "components/Input";
 import { FallingLines } from "react-loader-spinner";
@@ -32,12 +32,12 @@ function Home() {
           <LeftContainer>
             <RefreshButton onClick={refreshItems}>
               {itemLoading ? (
-                <FallingLines width="22px" height="22px" color="#5886c7" />
+                <FallingLines width='22px' height='22px' color='#5886c7' />
               ) : (
                 <img
-                  src="images/refresh.svg"
-                  height="10px"
-                  alt="refresh button"
+                  src='images/refresh.svg'
+                  height='10px'
+                  alt='refresh button'
                 />
               )}
             </RefreshButton>
@@ -47,20 +47,20 @@ function Home() {
               value={type}
               onChange={(e) => setType(e.target.value as ItemTypes)}
             >
-              <option value="all">유형 전체</option>
-              <option value="출석">출석</option>
-              <option value="과제">과제</option>
-              <option value="퀴즈">퀴즈</option>
+              <option value='all'>유형 전체</option>
+              <option value='출석'>출석</option>
+              <option value='과제'>과제</option>
+              <option value='퀴즈'>퀴즈</option>
             </Select>
             <EllipsisSelect
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
             >
-              <option value="all">과목 전체</option>
+              <option value='all'>과목 전체</option>
               {subjectList.map((title, idx) => {
                 return (
                   <option key={idx} value={title}>
-                    {title.split("[")[0]}
+                    {title.split("[")[0] || title}
                   </option>
                 );
               })}
